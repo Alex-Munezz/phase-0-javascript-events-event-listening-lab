65e62467b2f29629a66468b9fd845546d7b6ecf8
Alex <munezz456@gmail.com>
2023-03-25 17:24:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function addingEventListener() {
-        const input = document.getElementById('button');
+        const input = document.getElementById('input');
 input.addEventListener('click', function() {
   alert('I was clicked!');
 });
